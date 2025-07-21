3a1893176cce6ec12b7211d73d951c210c1eb9e7
Hawra Ayoob <hawraalkulaib.01@gmail.com>
2025-07-21 08:48:25 +0300

    commit message goes here

---
+++ homework/server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express(); 
+
+
+
+//greeting
+app.get('/greetings/:username', (req, res) => {
+    console.log(req.params.username)
+    res.send(`Hello there, ${req.params.username}! It's great to see you!`)
+});
+
+//rolling
+app.get('/roll/:number', (req,res)  =>{
+    const number= req.params.number;
+    const num= parseInt(number,10);
+    
+if (isNaN(num)) {
+        res.send("You must specify a number.");
+    } else {
+        const roll = Math.floor(Math.random() * (num + 1));
+        res.send(`You rolled a ${roll}.`);
+    }
+});
+
+
+
+// Q-three
+
+const collectibles = [
+  { name: 'shiny ball', price: 5.95 },
+  { name: 'autographed picture of a dog', price: 10 },
+  { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+];
+
+app.get('/collectibles/:index',(req,res) => {
+const index = parseInt(req.params.index, 10);
+
+if (index < 0 || index >= collectibles.length) {
+        res.send("This item is not yet in stock. Check back soon!");
+    } else {
+        const item = collectibles[index];
+        res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+    }
+});
+
+
+
+
+//Q four-filter
+
+  const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
+
+  app.get('/shoes',(req,res) => {
+
+    let filteredShoes =shoes;
+
+    if (req.query['min-price']) {
+        const minPrice = parseFloat(req.query['min-price']);
+        filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+    }
+
+
+    if (req.query['max-price']) {
+        const maxPrice = parseFloat(req.query['max-price']);
+        filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+    }
+
+
+
+     if (req.query['type']) {
+        const type = req.query['type'];
+        filteredShoes = filteredShoes.filter(shoe => shoe.type === type);
+    }
+
+    res.send(filteredShoes);
+    
+  });
+
+
+  //server
+  app.listen(3000, () =>{
+    console.log('Everything is gravy...');
+  });
+
+
